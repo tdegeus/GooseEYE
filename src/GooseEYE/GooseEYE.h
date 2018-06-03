8265dfd8ebb7d75a45484eeec718dc5687213446
Tom de Geus <tom@geus.me>
2018-06-03 12:00:35 +0200

    Simplified structure

--- src/GooseEYE/GooseEYE.h
+++ src/GooseEYE/GooseEYE.h
@@ -53,7 +53,51 @@ namespace GooseEYE
 
 // =================================================================================================
 
-#include "Ensemble.h"
+namespace GooseEYE {
+
+// -------------------------------------------------------------------------------------------------
+
+class Ensemble
+{
+private:
+
+  static const size_t   MAX_DIM=3;
+  cppmat::array<double> mData;
+  cppmat::array<double> mNorm;
+  std::vector<int>      mMid;
+  std::vector<int>      mSkip;
+  std::vector<size_t>   mPad;
+  bool                  mPeriodic;
+
+public:
+
+  // default constructor
+  Ensemble() = default;
+
+  // constructor
+  Ensemble(const std::vector<size_t> roi, bool periodic=true, bool zero_pad=false);
+
+  // get result
+  cppmat::array<double> result() const;
+
+  // 2-point correlation
+  void S2(const ArrS &f, const ArrS &g);
+  void S2(const ArrS &f, const ArrS &g, const ArrS &fmask, const ArrS &gmask);
+
+private:
+
+  // get the shape of "A" in "MAX_DIM" dimensions (padded with ones)
+  template<class T>
+  std::vector<int> shape(const cppmat::array<T> &A, const std::string &name="A") const;
+};
+
+// -------------------------------------------------------------------------------------------------
+
+} // namespace ...
+
+// =================================================================================================
+
+// #include "Ensemble.h"
 #include "Ensemble.hpp"
 #include "Ensemble_S2.hpp"
 
