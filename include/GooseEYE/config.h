441687b023c65b88961d92b2d641a6faef79a121
Tom de Geus <tdegeus@users.noreply.github.com>
2019-12-21 12:46:59 +0100

    Adding "W2c" (#42)
    * Adding "W2c"
    
    * Enabling example in CI
    
    * Bumping version

--- include/GooseEYE/config.h
+++ include/GooseEYE/config.h
@@ -46,23 +46,23 @@
 #include <xtensor/xadapt.hpp>
 #include <xtensor/xsort.hpp>
 
-// =================================================================================================
 
 #ifdef GOOSEEYE_ENABLE_ASSERT
 #define GOOSEEYE_ASSERT(expr) GOOSEEYE_ASSERT_IMPL(expr, __FILE__, __LINE__)
-#define GOOSEEYE_ASSERT_IMPL(expr, file, line)                                                                            \
-    if (!(expr))                                                                                                          \
-    {                                                                                                                     \
-        throw std::runtime_error(std::string(file) + ':' + std::to_string(line) + ": assertion failed (" #expr ") \n\t"); \
+#define GOOSEEYE_ASSERT_IMPL(expr, file, line) \
+    if (!(expr)) \
+    { \
+        throw std::runtime_error( \
+          std::string(file) + ':' + std::to_string(line) + \
+          ": assertion failed (" #expr ") \n\t"); \
     }
 #else
 #define GOOSEEYE_ASSERT(expr)
 #endif
 
-// =================================================================================================
 
 #define GOOSEEYE_VERSION_MAJOR 0
-#define GOOSEEYE_VERSION_MINOR 3
+#define GOOSEEYE_VERSION_MINOR 4
 #define GOOSEEYE_VERSION_PATCH 0
 
 #define GOOSEEYE_VERSION_AT_LEAST(x,y,z) \
@@ -75,10 +75,11 @@
      GOOSEEYE_VERSION_MINOR == y && \
      GOOSEEYE_VERSION_PATCH == z)
 
-// =================================================================================================
 
 #define SIGN(a)( (a<0) ? -1 : a>0 ? 1 : 0 )
 
-// =================================================================================================
+#define SIGN(a)( (a<0) ? -1 : a>0 ? 1 : 0 )
+// #define POS(a) ( (a<0) ?  0 : a           )
+
 
 #endif
