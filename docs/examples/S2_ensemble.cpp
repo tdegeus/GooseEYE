1a0a117fb2ef8c9f1884ee46831467a1e307e0cf
Tom de Geus <tom@geus.me>
2020-10-28 18:07:39 +0100

    Adding test

--- docs/examples/S2_ensemble.cpp
+++ docs/examples/S2_ensemble.cpp
@@ -1,18 +1,30 @@
-#include <xtensor/xarray.hpp>
-#include <xtensor/xio.hpp>
 #include <GooseEYE/GooseEYE.h>
+#include <xtensor.hpp>
+#include <highfive/H5Easy.hpp>
+
+#define MYASSERT(expr) MYASSERT_IMPL(expr, __FILE__, __LINE__)
+#define MYASSERT_IMPL(expr, file, line) \
+    if (!(expr)) { \
+        throw std::runtime_error( \
+            std::string(file) + ':' + std::to_string(line) + \
+            ": assertion failed (" #expr ") \n\t"); \
+    }
 
 int main()
 {
-    GooseEYE::Ensemble ensemble({11, 11});
+    GooseEYE::Ensemble ensemble({101, 101});
 
     for (size_t i = 0; i < 5; ++i)
     {
-        xt::xarray<int> I = GooseEYE::dummy_circles({50, 50});
+        auto I = GooseEYE::dummy_circles({200, 200});
         ensemble.S2(I, I);
     }
 
-    xt::xarray<double> S2 = ensemble.result();
+    auto S2 = ensemble.result();
+
+    // check against previous versions
+    H5Easy::File data("S2_ensemble.h5", H5Easy::File::ReadOnly);
+    MYASSERT(xt::allclose(S2, H5Easy::load<decltype(S2)>(data, "S2")));
 
     return 0;
 }
