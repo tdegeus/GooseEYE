f2b90ec0593cd2b71be8d0e06456429442786aa2
Tom de Geus <tom@geus.me>
2023-07-05 08:57:16 +0200

    Updating setup

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ project_name = "GooseEYE"
 setup(
     name=project_name,
     description="Image based geometrical statistics",
-    long_description="Image based geometrical statistics",
     version=get_version(),
     license="GPLv3",
     author="Tom de Geus",
@@ -15,5 +14,6 @@ setup(
     packages=[f"{project_name}"],
     package_dir={"": "python"},
     cmake_install_dir=f"python/{project_name}",
-    cmake_minimum_required_version="3.13...3.21",
+    cmake_minimum_required_version="3.13",
+    python_requires=">=3.6",
 )
